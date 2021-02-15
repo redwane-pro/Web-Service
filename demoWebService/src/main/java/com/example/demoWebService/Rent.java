a822e987189193b6d8b1ba90027dda67357cb382
redwane-pro <74648731+redwane-pro@users.noreply.github.com>
2021-02-15 17:39:28 +0100

    Add files via upload

---
+++ demoWebService/src/main/java/com/example/demoWebService/Rent.java
@@ -0,0 +1,28 @@
+package com.example.demoWebService;
+
+public class Rent {
+
+    private String begin;
+    private String end;
+
+    public Rent(String begin, String end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public String getBegin() {
+        return begin;
+    }
+
+    public void setBegin(String begin) {
+        this.begin = begin;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+}
